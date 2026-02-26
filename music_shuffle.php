f4cd98b75d66f2f7858f95902fd150b73a21f4e6
marinostrus <marijn@marijnvanhoudt.be>
2026-02-26 17:41:33 +0100

    Update

--- music_shuffle.php
+++ music_shuffle.php
@@ -1,11 +1,9 @@
 <?php
-$song = $_GET["id"];
-
 // KILL PROCESS 
 exec('sudo killall -9 mpg123');
 
 // PLAY SONG
-exec('sudo /usr/bin/screen -d -m /usr/bin/mpg123 -z /var/www/html/music/*');
+exec('sudo mpg123 -z /var/www/html/music/*');
 
 // REDIRECT
 $redirect = "music_collection.php";
